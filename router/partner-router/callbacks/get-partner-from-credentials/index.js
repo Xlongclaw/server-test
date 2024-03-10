3cac9d465e81fe3de929c2a2998e309d19fe6bbc
unknown <xlongclaw@gmail.com>
2024-03-10 15:45:44 +0530

    fixed /partner api

--- router/partner-router/callbacks/get-partner-from-credentials/index.js
+++ router/partner-router/callbacks/get-partner-from-credentials/index.js
@@ -9,29 +9,36 @@ const partnerModel = require("../../../../database/models/partnerModel");
  * @param {*} response
  */
 const getUserFromCredentials = async (request, response) => {
-  /**
-   * storing phoneNumber and params got from the request query params.
-   */
-  const { phoneNumber, password } = request.query;
+  try{
 
-  /**
-   * Serching for the partner in the database whose phoneNumber Matches
-   * with the request param query phoneNumber.
-   */
-  const partner = await partnerModel.findOne({ phoneNumber });
-
-  /**
-   * If the partner with the phone Number is found then comparing the passwords.
-   */
-  if (partner) {
-    if (partner.password === password) {
-      /**
-       * Generating an authToken for partner so that the partner can stay authenticated.
-       */
-      const userToken = generateToken(request.query.phoneNumber);
-      response.status(200).json({ code: "SUCCESS", partner, userToken });
-    } else response.status(400).json({ code: "PASSWORD_DOES_NOT_MATCH" });
-  } else response.status(401).json({ code: "PARTNER_DOES_NOT_EXIST" });
+    /**
+     * storing phoneNumber and params got from the request query params.
+     */
+    const { phoneNumber, password } = request.query;
+  
+    /**
+     * Serching for the partner in the database whose phoneNumber Matches
+     * with the request param query phoneNumber.
+     */
+    const partner = await partnerModel.findOne({ phoneNumber });
+  
+    /**
+     * If the partner with the phone Number is found then comparing the passwords.
+     */
+    if (partner) {
+      if (partner.password === password) {
+        /**
+         * Generating an authToken for partner so that the partner can stay authenticated.
+         */
+        const userToken = generateToken(request.query.phoneNumber);
+        response.status(200).json({ code: "SUCCESS", partner, userToken });
+      } else response.status(400).json({ code: "PASSWORD_DOES_NOT_MATCH" });
+    } else response.status(401).json({ code: "PARTNER_DOES_NOT_EXIST" });
+  }
+  catch(err){
+    console.err(err);
+    response.status(402).json({ code: "SOMETHING_WENT_WROGN" });
+  }
 };
 
 module.exports = getUserFromCredentials;
