3ef729dc702bbd224d58b33b9cfb1353ccf4ee16
unknown <xlongclaw@gmail.com>
2024-03-10 15:14:35 +0530

    fixed partner/add api

--- router/partner-router/callbacks/add-partner/index.js
+++ router/partner-router/callbacks/add-partner/index.js
@@ -12,52 +12,58 @@ const verifyToken = require("../../../../utils/verifyToken");
  * @param {*} response
  */
 const addPartner = async (request, response) => {
-  /**
-   * Verifying the token by passing it in verifyToken function.
-   */
-  const tokenData = verifyToken(request.body.userToken);
 
-  /**
-   * If the token Verification is successful addind user data to the
-   * database.
-   */
-  if (tokenData.status == "VERIFIED") {
-    console.log(JSON.stringify(request.body));
-    const {
-      address,
-      description,
-      foodTags,
-      restaurantTags,
-      minPrepTime,
-      maxPrepTime,
-      name,
-      images,
-    } = request.body.restaurantData;
+  try{
 
-    await restaurantModel.create({
-      address,
-      description,
-      foodTags,
-      images,
-      restaurantTags,
-      name,
-      maxPrepTime,
-      minPrepTime,
-      rating: 0,
-      foodCategoryIds: [],
-      isOpen: false,
-    }).then((res)=>console.log(res))
-
-
-    await partnerModel.create({
-      name: request.body.name,
-      password: request.body.password,
-      phoneNumber: tokenData.data,
-      restaurantId:''
-    });
-    response.status(200).json({ code: "SUCCESS" });
-  } else {
-    response.status(400).json({ code: "INVALID_TOKEN" });
+    /**
+     * Verifying the token by passing it in verifyToken function.
+     */
+    const tokenData = verifyToken(request.body.userToken);
+  
+    /**
+     * If the token Verification is successful addind user data to the
+     * database.
+     */
+    if (tokenData.status == "VERIFIED") {
+      const {
+        address,
+        description,
+        foodTags,
+        restaurantTags,
+        minPrepTime,
+        maxPrepTime,
+        name,
+        images,
+      } = request.body.restaurantData;
+  
+      const restaurant = await restaurantModel.create({
+        address,
+        description,
+        foodTags,
+        images,
+        restaurantTags,
+        name,
+        maxPrepTime,
+        minPrepTime,
+        rating: 0,
+        foodCategoryIds: [],
+        isOpen: false,
+      })
+  
+      await partnerModel.create({
+        name: request.body.name,
+        password: request.body.password,
+        phoneNumber: tokenData.data,
+        restaurantId:restaurant._id
+      });
+      response.status(200).json({ code: "SUCCESS" });
+    } else {
+      response.status(400).json({ code: "INVALID_TOKEN" });
+    }
+  }
+  catch(err){
+    console.log(err);
+    response.status(401).json({ code: "SOMETHING_WENT_WRONG" });
   }
 };
 
