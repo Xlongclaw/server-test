4277e5ff6e14339b12b2da4113822470fcfb71c2
unknown <xlongclaw@gmail.com>
2024-03-15 18:21:15 +0530

    fixed /user/basket get

--- router/user-router/callbacks/get-basket/index.js
+++ router/user-router/callbacks/get-basket/index.js
@@ -1,13 +1,21 @@
 const userModel = require("../../../../database/models/userModel");
 const verifyToken = require("../../../../utils/verifyToken");
 
-
 const getBasket = async (request, response) => {
   const tokenData = verifyToken(request.query.userToken);
 
   if (tokenData.status == "VERIFIED") {
     const data = await userModel
-      .find({ phoneNumber: tokenData.data }).select("basket")
+      .findOne(
+        {
+          phoneNumber: tokenData.data,
+        },
+        {
+          _id:0,
+          basket: { $elemMatch: { restaurantId: request.query.restaurantId } },
+        }
+      )
+      .select("basket")
       .catch(() => {
         response.status(401).json({ code: "NOT_FOUND" });
       });
